a65498f86eb4c78b7f2f75aefd18f0408fe44d7c
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-08-31 11:00:37 -0700

    try fixing dir

--- ci/asdf-setup-go.sh
+++ ci/asdf-setup-go.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 set -euxo pipefail
 
-ASDF_GOLANG_DIRECTORY="$(asdf where golang)"
+ASDF_GOLANG_DIRECTORY="$(asdf where golang)/go"
 
 # https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-environment-variable
 echo "::set-env name=GOROOT::${ASDF_GOLANG_DIRECTORY}"
