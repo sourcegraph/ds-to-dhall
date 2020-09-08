8e7723b615184c1c2335e51fb8da0261d06aef6a
uwedeportivo <534011+uwedeportivo@users.noreply.github.com>
2020-09-08 13:36:49 -0700

    derive component from sourcegraph-component label if present (#11)
    * derive component from source-component label if present
    
    * switched to standard k8s label already available
    
    * handle manifests without labels section
    
    * flexible inputs and option to save dhall type of output record (#12)
    
    * flexible inputs and option to save dhall type of output record
    
    * test for common prefix
    
    * add bool flag that controls if output has full k8s schema records or not (#13)
    
    * add bool flag that controls if output has full k8s schema records or not
    
    * delete record.yaml (added by mistake)
    
    * Ignore files list (#14)
    
    * flag specifies which input files to ignore
    
    * record.yaml is everywhere in my stacked PRs
    
    * now delete for real
    
    * code review geoffrey
    
    * option to transform certain lists to records (#15)
    
    * option to transform certain lists to records
    
    * delete mistaken output file
    
    * break out docker image spec string into a record of parts (#16)
    
    * break out docker image spec string into a record of parts
    
    * transform simplified schema with optionals (#17)
    
    * transform simplified schema with optionals
    
    * ignore struct tag lint error for dhall types grammar definition
    
    * ignore struct tag lint error for dhall types grammar definition 2
    
    * output schema file and dhall format all outputs (#18)

---
+++ common_prefix_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func executeTestCommonRoot(pa, pb, expected string, t *testing.T) {
+	c, err := commonPrefix([]string{pa, pb})
+	if err != nil {
+		t.Errorf("error while computing commong prefix for a = %s, b = %s: %v", pa, pb, err)
+		return
+	}
+
+	if c != expected {
+		t.Errorf("expected = %s, got = %s;     a = %s, b = %s", expected, c, pa, pb)
+	}
+}
+
+func TestCommonPrefix(t *testing.T) {
+	executeTestCommonRoot("/a/b/c/d/e/f", "/a/b/v", "/a/b", t)
+	executeTestCommonRoot("/a/b/v", "/a/b/c/d/e/f", "/a/b", t)
+	executeTestCommonRoot("/a", "/b", "/", t)
+	executeTestCommonRoot("/a/b", "/a/b", "/a/b", t)
+	executeTestCommonRoot("/a/b/c", "/a/b/d", "/a/b", t)
+	executeTestCommonRoot("/a", "/a", "/a", t)
+	executeTestCommonRoot("/a/c", "/a/b", "/a", t)
+	executeTestCommonRoot("/a/b/c", "/a/b", "/a/b", t)
+	executeTestCommonRoot("/a/b", "/a/b/c", "/a/b", t)
+	executeTestCommonRoot("/a/b/v/", "/a/b/v", "/a/b/v", t)
+	executeTestCommonRoot("/a", "/", "/", t)
+}
